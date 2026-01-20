086e5ade5ce2e798e5b08e900b02a6fafd73dfa9
Quibylix <barraza.fredimanuel@gmail.com>
2026-01-20 14:06:15 -0600

    feat: configure nvim-dap for debugging

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,33 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    local dap = require("dap")
+    local ui = require("dapui")
+
+    ui.setup()
+
+    require("mason-nvim-dap").setup({
+      ensure_installed = { "codelldb" },
+      handlers = {},
+    })
+
+    -- Abrir/Cerrar UI automáticamente al iniciar/terminar debug
+    dap.listeners.before.attach.dapui_config = function() ui.open() end
+    dap.listeners.before.launch.dapui_config = function() ui.open() end
+    dap.listeners.before.event_terminated.dapui_config = function() ui.close() end
+    dap.listeners.before.event_exited.dapui_config = function() ui.close() end
+
+    -- Keymaps
+    vim.keymap.set("n", "<F5>", dap.continue)
+    vim.keymap.set("n", "<F10>", dap.step_over)
+    vim.keymap.set("n", "<F11>", dap.step_into)
+    vim.keymap.set("n", "<F12>", dap.step_out)
+    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+  end,
+}
