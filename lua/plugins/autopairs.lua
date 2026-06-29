ec22758f31db470e39415b4790879dbbb6de4a59
Quibylix <barraza.fredimanuel@gmail.com>
2026-06-28 22:18:55 -0600

    refactor(autopairs): use explicit keymaps for nvim-surround

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -16,14 +16,11 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
-    opts = {
-      keymaps = {
-        normal = "<leader>s",
-        normal_cur = "<leader>ss",
-        visual = "<leader>s",
-        delete = "<leader>sd",
-        change = "<leader>sc",
-      }
-    },
+    config = function()
+      vim.api.nvim_set_keymap('n', 's', '<Plug>(nvim-surround-normal)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('v', 's', '<Plug>(nvim-surround-visual)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'sd', '<Plug>(nvim-surround-delete)', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'sc', '<Plug>(nvim-surround-change)', { noremap = true, silent = true })
+    end,
   }
 }
