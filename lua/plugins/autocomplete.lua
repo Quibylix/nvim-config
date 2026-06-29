5150ec54548d1470e01febbcf21c81375dea1050
Quibylix <barraza.fredimanuel@gmail.com>
2026-06-29 16:07:58 -0600

    feat(completion): migrate from nvim-cmp to blink.cmp

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,85 +1,49 @@
-local init = function()
-  local cmp = require("cmp")
-  local luasnip = require("luasnip")
-
-  local cmp_enter_mapping = cmp.mapping(function(fallback)
-    if cmp.visible() == false then
-      fallback()
-    elseif luasnip.expandable() then
-      luasnip.expand()
-    elseif cmp.get_active_entry() then
-      cmp.confirm({ select = false })
-    else
-      fallback()
-    end
-  end)
-
-  local cmp_tab_mapping = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      return cmp.select_next_item()
-    else
-      fallback()
-    end
-  end, { "i", "s" })
-
-  local cmp_s_tab_mapping = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      return cmp.select_prev_item()
-    else
-      fallback()
-    end
-  end, { "i", "s" })
-
-  cmp.setup({
-    preselect = cmp.PreselectMode.None,
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body) -- For Luasnip
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-      end,
-    },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<cr>"] = cmp_enter_mapping,
-      ["<tab>"] = cmp_tab_mapping,
-      ["<s-tab>"] = cmp_s_tab_mapping,
-    }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "luasnip" }, -- For luasnip users.
-    }, {
-      { name = "buffer" },
-    })
-  })
-  cmp.setup.cmdline({ "/", "?" }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = "buffer" }
-    }
-  })
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" }
-    }, {
-      { name = "cmdline" }
-    }),
-    matching = { disallow_symbol_nonprefix_matching = false }
-  })
-end
-
 return {
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "hrsh7th/cmp-cmdline" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "saadparwaiz1/cmp_luasnip" },
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "saadparwaiz1/cmp_luasnip", "L3MON4D3/LuaSnip", "neovim/nvim-lspconfig" },
-    init = init,
+    "saghen/blink.cmp",
+    dependencies = { "saghen/blink.lib", "rafamadriz/friendly-snippets" },
+    build = function()
+      require('blink.cmp').build():pwait()
+    end,
+    opts = {
+      keymap = {
+        preset = "none",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "snippet_forward", "fallback" },
+        ["<C-p>"] = { "snippet_backward", "fallback" },
+      },
+
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+        menu = { border = "rounded" },
+        documentation = {
+          window = { border = "rounded" },
+        },
+      },
+      snippets = { preset = "default" },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      cmdline = {
+        enabled = true,
+        keymap = { preset = 'inherit' },
+        completion = {
+          list = {
+            selection = {
+              preselect = false,
+              auto_insert = true,
+            },
+          },
+          menu = { auto_show = true }
+        },
+      },
+    },
   },
 }
